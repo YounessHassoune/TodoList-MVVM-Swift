f039a0c53dd08f581ccc1f3658bcb58ad5dca8ae
YounessHassoune <yhassoune7@gmail.com>
2021-11-12 10:07:41 +0100

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Youness Hassoune on 12/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
